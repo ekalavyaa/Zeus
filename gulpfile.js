88b76701a6c890ad1c0cfd1aab859b29d60f8427
Antony <antony.fancis@simplogics.com>
2017-11-13 17:51:59 +0530

    new gulp task added

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,32 @@
-var gulp = require('gulp');
-var webpack = require('webpack-stream');
+let gulp = require('gulp');
+let webpack = require('webpack-stream');
 const eslint = require('gulp-eslint');
-var gulpif = require('gulp-if');
-var args = require('yargs').argv;
-var gulpprint = require('gulp-print');
-var spawn = require('child_process').spawn;
+let gulpif = require('gulp-if');
+let args = require('yargs').argv;
+let gulpprint = require('gulp-print');
+let spawn = require('child_process').spawn;
 
 
-gulp.task('build',['eslint'], function() {
+gulp.task('build',['eslint','copy-config'] , () => {
 	return gulp.src('index.js')
     .pipe(webpack( require('./webpack.config.js') ))
     .pipe(gulp.dest('build/'));
 });
 
 gulp.task('eslint', () => (
-    gulp.src(['**/*.js', '!node_modules/**', '!src/**'])
+    gulp.src(['**/*.js', '!node_modules/**', '!build/**'])
     .pipe(gulpif(args.verbose, gulpprint()))
     .pipe(eslint({ fix: true }))
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
 ));
 
-gulp.task('serve',['eslint'], function() {
+gulp.task('serve',['eslint'], () => {
   spawn('node', ['index.js'], { stdio: 'inherit' });
+});
+
+gulp.task('copy-config', () => {
+    var config =  gulp.src(['config/**/*']).pipe(gulp.dest('build/config'));
+    var packageJson = gulp.src(['package.json']).pipe(gulp.dest('build'));
+    return Promise.all([config, packageJson]);
 });
